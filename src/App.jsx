1c236deb6b42d971f8f1d38852ec1b4806076742
Dipanjan Chowdhury <chowdhurydipanjan173@gmail.com>
2026-07-31 23:25:48 +0530

    create the skeleton of the webpage

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const App = () => {
+  const [todos, setTodos] = useState([]);
+  let newTodos = todos;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    newTodos.push(todos);
+    setTodos(newTodos);
+  };
+
   return (
     <div>
-      
+      <h1>Todo App</h1>
+      <form
+        onSubmit={(e) => {
+          handleSubmit(e);
+        }}
+        action=""
+      >
+        <input
+          value={todos}
+          type="text"
+          placeholder="Add a new todo"
+          onChange={(e) => {
+            setTodos(e.target.value);
+          }}
+        />
+        <button>Add</button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
